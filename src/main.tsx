8dae1044b93ed5f2f7bec370489a1cff234b1407
Matthew Derry <matthew.derry@Splendidunlimited.com>
2024-02-14 10:40:01 +0000

    split css for game css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./App.css";
+import "./Game.css";
 
 createRoot(document.getElementById("smc-game") as HTMLElement).render(<App />);
